60132215aa1672ea3bc063c05edba96cd804e495
rebeca <ximenareberamirez@gmail.com>
2022-09-18 23:32:40 -0300

    clase 11 ejercitación

---
+++ java/script.js
@@ -0,0 +1,17 @@
+let diadelasemana = prompt("¿Qué día es hoy?")
+switch (diadelasemana) {
+    case "lunes":
+        alert("20 minutos de entrada en calor, Talones a cola, sentadillas, skiping y estocada, 3 series de 10, con un desacanso de 10 minuttos entre serie "); break
+    case "martes":
+        alert("20 minutos de entrada en calor, Talones a cola, sentadillas, escaladas, abdominales cortos3, series de 15, con un desacanso de 10 minuttos entre serie "); break
+    case "miercoles":
+        alert("20 minutos de entrada en calor, Talones a cola, sentadillas, escaladas, abdominales cortos3,abdominales bicicleta, series de 20, con un desacanso de 10 minuttos entre serie "); break
+    case "jueves":
+        alert("20 minutos de entrada en calor, Talones a cola, sentadillas, escaladas, abdominales cortos3, series de 25, con un desacanso de 10 minuttos entre serie y plancla de 1 minuto  "); break
+    case "viernes":
+        alert("20 minutos de entrada en calor, Talones a cola, sentadillas, escaladas, abdominales cortos, 3series de 30, con un desacanso de 10 minuttos entre serie y plancha de un minuto"); break
+
+    default:
+        alert("escribe el dia de la semana con letras minusculas")
+
+}
---
+++ java/script1.js
@@ -0,0 +1,3 @@
+for (i = 20; i <= 70; i++) {
+    document.write("El número es:" + i + "<br>");
+}
